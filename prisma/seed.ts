82583f5f835a1f1633d161c7760e47852bde6df7
lemon7h3chick <312019530+lemon7h3chick@users.noreply.github.com>
2026-09-26 13:00:47 +0800

    Add seed script for Program and Student
    Add constant programNames list of available programs, convert these
    names into objects as programsData. Use programsData as data for
    createMany(), so prisma will create these values into your database and
    add auto increment id to each program name. Get the programs on your
    databse using findMany(), this will return objects with program id and
    name, so we can assign these ids to student's programId.
    
    Add constant STUDENTS_PER_PROGRAM as number of students in a program.
    
    On each program create a size STUDENTS_PER_PROGRAM Array of objects with
    fake name and its programId.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,31 @@
+import { faker } from '@faker-js/faker';
 import { PrismaService } from '../src/prisma/prisma.service';
 
 const prisma = new PrismaService();
+const STUDENTS_PER_PROGRAM = 10;
 
-async function main() {}
+async function main() {
+  const programNames = [
+    'BS Information Technology',
+    'BS Computer Science',
+    'BS Information Systems',
+  ];
+
+  const programsData = programNames.map((name) => ({ name }));
+
+  await prisma.program.createMany({ data: programsData });
+  const programs = await prisma.program.findMany();
+
+  const studentsData = programs.flatMap((program) =>
+    Array.from({ length: STUDENTS_PER_PROGRAM }, () => ({
+      name: faker.person.fullName(),
+      programId: program.id,
+    })),
+  );
+
+  await prisma.student.createMany({ data: studentsData });
+  const students = await prisma.student.findMany();
+}
 
 main()
   .catch((e) => console.error(e))
