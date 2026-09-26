3f0ec6de7c294306c4c6c7ef6182eab544fb3f58
lemon7h3chick <312019530+lemon7h3chick@users.noreply.github.com>
2026-09-26 12:46:09 +0800

    Add function main() for seeding logic
    Catch errors and disconnect before exit.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,3 +1,9 @@
 import { PrismaService } from '../src/prisma/prisma.service';
 
 const prisma = new PrismaService();
+
+async function main() {}
+
+main()
+  .catch((e) => console.error(e))
+  .finally(async () => await prisma.$disconnect());
